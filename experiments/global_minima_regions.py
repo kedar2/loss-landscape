47e0448afdc6190b732d7a9898163f7fb0350439
Kedar Karhadkar <kedarkarhadkar@gmail.com>
2023-08-09 16:12:05 +0200

    Add method to check probability of global minima

--- experiments/global_minima_regions.py
+++ experiments/global_minima_regions.py
@@ -14,7 +14,10 @@ from utils import generate_heatmap
 from math import ceil
 from scipy.optimize import linprog
 
-def sample_activation_region(n: int=100, input_dim: int=100, hidden_dim: int=12) -> np.array:
+def sample_activation_region(n: int=100,
+                             input_dim: int=100,
+                             hidden_dim: int=12,
+                             X: torch.tensor=None) -> np.array:
     """
     Given specifications for an MLP, sample a random data point and initialization
     and compute the activation matrix A for the region.
@@ -23,6 +26,7 @@ def sample_activation_region(n: int=100, input_dim: int=100, hidden_dim: int=12)
         n (int): The number of samples in the dataset.
         input_dim (int): The dimension of the input data.
         hidden_dim (int): The dimension of the hidden layer.
+        X (torch.tensor): The input data. If None, sample a random dataset.
     
     Returns:
         A (np.array): The activation matrix.
@@ -31,14 +35,18 @@ def sample_activation_region(n: int=100, input_dim: int=100, hidden_dim: int=12)
     """
 
     model = models.MLP(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=1, num_hidden_layers=1)
-    X = torch.rand(n, input_dim)
+    if X is None:
+        X = 2 * torch.rand(n, input_dim) - 1
     _ = model(X)
     X = X.numpy()
     A = model.activations[0].float().numpy()
     v = model.last_layer.view(-1).numpy()
     return A, X, v
 
-def random_region_global_minima(n: int=2, input_dim: int=100, hidden_dim: int=12) -> bool:
+def random_region_global_minima(n: int=2,
+                                input_dim: int=100,
+                                hidden_dim: int=12,
+                                X: torch.tensor=None) -> bool:
     """
     Given specifications for an MLP, sample a random data point and initialization
     and determine whether the region contains a zero loss global minimum.
@@ -54,12 +62,13 @@ def random_region_global_minima(n: int=2, input_dim: int=100, hidden_dim: int=12
         n (int): The number of samples in the dataset.
         input_dim (int): The dimension of the input data.
         hidden_dim (int): The dimension of the hidden layer.
+        X (torch.tensor): The input data. If None, sample a random dataset.
     
     Returns:
         (bool): Whether the region contains a global minima.
     """
 
-    A, X, v = sample_activation_region(n=n, input_dim=input_dim, hidden_dim=hidden_dim)
+    A, X, v = sample_activation_region(n=n, input_dim=input_dim, hidden_dim=hidden_dim, X=X)
     y = np.random.randn(n) # Random outputs
     
     X = np.hstack((X, np.ones((n, 1)))) # Add bias term to X
@@ -80,6 +89,32 @@ def random_region_global_minima(n: int=2, input_dim: int=100, hidden_dim: int=12
     
     return res.success
 
+def prob_global_minima(n: int=2,
+                       input_dim: int=100,
+                       hidden_dim: int=12,
+                       num_trials: int=100,
+                       X: torch.tensor=None) -> float:
+    """
+    Given specifications for an MLP, compute the probability that a random region
+    contains a zero loss global minimum.
+
+    Args:
+        n (int): The number of samples in the dataset.
+        input_dim (int): The dimension of the input data.
+        hidden_dim (int): The dimension of the hidden layer.
+        num_trials (int): The number of trials to simulate.
+        X (torch.tensor): The input data. If None, sample a random dataset.
+    
+    Returns:
+        (float): The probability that a random region contains a global minima.
+    """
+
+    num_global_minima = 0
+    for _ in range(num_trials):
+        if random_region_global_minima(n=n, input_dim=input_dim, hidden_dim=hidden_dim, X=X):
+            num_global_minima += 1
+    return num_global_minima / num_trials
+
 if __name__ == "__main__":
-    exists = random_region_global_minima(n=1, input_dim=1, hidden_dim=1)
-    print(exists)
+    p = prob_global_minima(n=10, input_dim=20, hidden_dim=10)
+    print(p)
